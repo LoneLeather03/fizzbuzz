98c6be3beba352f1c44ed934cca9909c5c39ac8b
User <User@User-PC.admin.ad.cnm.edu>
2017-06-13 16:23:12 -0600

    exception handling on fizzbuzz

--- src/fizzbuzz/FizzBuzz.java
+++ src/fizzbuzz/FizzBuzz.java
@@ -2,15 +2,27 @@
 package fizzbuzz;
 
 public class FizzBuzz {
+  
   public static final int DEFAULT_UPPER_LIMIT = 100;
 
-
+  public static final String USAGE =
+      "Usage:%n" + "\tjava [options] fizzbuzz.FizzBuzz [limit]%n" + "%n" + "where%n"
+          + "\toptions = VM otions%n" + "\tlimit = positive upper counting limit (default = 100)%n";
 
   public static void main(String[] args) {
-    int upperLimit = (args.length > 0) ? Integer.parseInt(args[0]) : DEFAULT_UPPER_LIMIT;
-    emitFizzBuzz(upperLimit);
+    try {
+      int upperLimit = (args.length > 0) ? Integer.parseInt(args[0]) : DEFAULT_UPPER_LIMIT;
+      if (upperLimit < 0) {
+        throw new IllegalArgumentException("Upper limit must be positive");
+      }
+      emitFizzBuzz(upperLimit);
+    }
+    catch (IllegalArgumentException ex) {
+      emitUsage();
+      ex.printStackTrace();
+    }
   }
-
+  
   private static void emitFizzBuzz(int limit) {
     for (int i = 1; i <= limit; i++) {
       String response = "";
@@ -20,11 +32,15 @@ public class FizzBuzz {
       if (i % 5 == 0) {
         response += "Buzz";
       }
-      if (response.length() == 0)
+      if (response.length() == 0) {
         response = String.format("%d", i);
-
+      }
       System.out.println(response);
     }
-
   }
+
+  private static void emitUsage() {
+    System.out.printf(USAGE);  
+  }
+  
 }
