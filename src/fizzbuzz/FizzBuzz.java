e585dbb8f46993a8a8d06cb3793a94b6feed7210
User <User@User-PC.admin.ad.cnm.edu>
2017-06-13 15:15:31 -0600

    first running fizzbuzz

---
+++ src/fizzbuzz/FizzBuzz.java
@@ -0,0 +1,30 @@
+
+package fizzbuzz;
+
+public class FizzBuzz {
+  public static final int DEFAULT_UPPER_LIMIT = 100;
+
+
+
+  public static void main(String[] args) {
+    int upperLimit = (args.length > 0) ? Integer.parseInt(args[0]) : DEFAULT_UPPER_LIMIT;
+    emitFizzBuzz(upperLimit);
+  }
+
+  private static void emitFizzBuzz(int limit) {
+    for (int i = 1; i <= limit; i++) {
+      String response = "";
+      if (i % 3 == 0) {
+        response += "Fizz";
+      }
+      if (i % 5 == 0) {
+        response += "Buzz";
+      }
+      if (response.length() == 0)
+        response = String.format("%d", i);
+
+      System.out.println(response);
+    }
+
+  }
+}
